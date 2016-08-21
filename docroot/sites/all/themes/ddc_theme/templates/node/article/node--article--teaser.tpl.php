07c778f5eb88b2495efa8a48b4816f77c3b7b3bd
Mark Pavlitski <markpavlitski@gmail.com>
2016-08-21 18:50:38 +0100

    Added subtitle to Article teaser.

--- docroot/sites/all/themes/ddc_theme/templates/node/article/node--article--teaser.tpl.php
+++ docroot/sites/all/themes/ddc_theme/templates/node/article/node--article--teaser.tpl.php
@@ -14,6 +14,7 @@
         </div>
         <div class="media-body">
           <h1><?php print $linked_title; ?></h1>
+          <h2><?php print render($content['field_subtitle']); ?></h2>
           <?php print render($content['body']); ?>
         </div>
       </div>
